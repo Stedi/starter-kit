e16756dc0111813648e08b69c1f390c7aca12997
Joost Ronkes Agerbeek <joost.agerbeek@stedi.com>
2021-10-07 15:26:31 +0200

    feat: read files from directory

--- translate-edi/translate.mjs
+++ translate-edi/translate.mjs
@@ -1,33 +1,43 @@
-import { readFile } from "fs/promises"
+import { readFile, readdir } from "fs/promises"
 import { request } from "https"
 
+const path = "./invoices";
 const url = "https://edi-core.stedi.com/2021-06-05/translate";
-const body = JSON.stringify({
-  input_format: "edi",
-  output_format: "jedi@1.0",
-  input: await readFile("example.x12", "ascii")
-})
-const options = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Content-Length": body.length,
-    "Authorization": `Key ${process.env.STEDI_API_KEY}`
-  }
-}
 
-const req = request(url, options, response => {
-  console.log(response.statusCode);
-
-  response.on("data", data => {
-    const responseBody = JSON.parse(data);
-    console.log(JSON.stringify(responseBody.output));
+const files = await readdir(path);
+for (const file of files) {
+  const body = JSON.stringify({
+    input_format: "edi",
+    output_format: "jedi@1.0",
+    input: await readFile(`${path}/${file}`, "ascii")
   })
-})
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Content-Length": body.length,
+      "Authorization": `Key ${process.env.STEDI_API_KEY}`
+    }
+  }
 
-req.on("error", error => {
-  console.error(error);
-})
+  const req = request(url, options, response => {
+    if (response.statusCode === 200) {
+      response.on("data", data => {
+        const responseBody = JSON.parse(data);
+        console.log(JSON.stringify(responseBody.output));
+      })
+    }
+    else {
+      response.on("data", data => {
+        console.log(response.statusCode + " " + data);
+      })
+    }
+  })
 
-req.write(body);
-req.end();
+  req.on("error", error => {
+    console.error(error);
+  })
+
+  req.write(body);
+  req.end();
+}
