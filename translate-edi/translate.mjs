b567e152c827e8a9965154ba7dae208e16410b0c
Joost Ronkes Agerbeek <joost.agerbeek@stedi.com>
2021-10-07 19:05:18 +0200

    feat: sum amount of all invoices

--- translate-edi/translate.mjs
+++ translate-edi/translate.mjs
@@ -4,8 +4,13 @@ import { request } from "https"
 const path = "./invoices";
 const url = "https://edi-core.stedi.com/2021-06-05/translate";
 
+let totalAmount = 0;
+let openRequests = 0;   // TODO: Find a better way of handling async.
+
 const files = await readdir(path);
 for (const file of files) {
+  openRequests += 1
+
   const body = JSON.stringify({
     input_format: "edi",
     output_format: "jedi@1.0",
@@ -21,17 +26,26 @@ for (const file of files) {
   }
 
   const req = request(url, options, response => {
-    if (response.statusCode === 200) {
-      response.on("data", data => {
-        const responseBody = JSON.parse(data);
-        console.log(JSON.stringify(responseBody.output));
-      })
-    }
-    else {
-      response.on("data", data => {
+    response.on("data", data => {
+      const responseBody = JSON.parse(data);
+      if (response.statusCode == 200) {
+        if (responseBody.code == "valid") {
+          // TODO: Figure out if this can lead to a race condition.
+          totalAmount += parseInt(responseBody.output.interchanges[0].groups[0].transaction_sets[0].summary["0100_TDS"]["01"]);
+        }
+        else {
+          console.warn(`Skipping ${file} because it failed validation.`);
+        }
+      }
+      else {
         console.log(response.statusCode + " " + data);
-      })
-    }
+      }
+
+      // TODO: Find a better way of waiting for all requests to be done.
+      if (--openRequests == 0) {
+        console.log(totalAmount);
+      }
+    })
   })
 
   req.on("error", error => {
